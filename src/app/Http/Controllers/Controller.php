003002fd2aa2f6cc9685fe9b2fdf3ee99e9ef55d
Rodrigo Battagliero <rbattagliero@gmail.com>
2024-12-11 15:21:24 -0300

    Add common methods

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+
 abstract class Controller
 {
-    //
+    public static function rollback($e, $message ="Something went wrong!.") {
+        //DB::rollBack();
+        self::throw($e, $message);
+    }
+
+    public static function throw($e, $message ="Something went wrong!.") {
+        //Log::info($e);
+        throw new HttpResponseException(response()->json(["message"=> $message], 500));
+    }
+
+    public static function sendResponse($result , $message ,$code=200) {
+        $response=[
+            'success' => true,
+            'data'    => $result
+        ];
+        if(!empty($message)){
+            $response['message'] =$message;
+        }
+        return response()->json($response, $code);
+    }
 }

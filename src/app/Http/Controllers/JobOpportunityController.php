8f31511124fe6dfc3a207b7de4e185badd51566d
Rodrigo Battagliero <rbattagliero@gmail.com>
2024-12-11 03:47:55 -0300

    Job Opportunties clases generated

---
+++ src/app/Http/Controllers/JobOpportunityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreJobOpportunityRequest;
+use App\Http\Requests\UpdateJobOpportunityRequest;
+use App\Models\JobOpportunity;
+
+class JobOpportunityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJobOpportunityRequest $request)
+    {
+        //
+    }
+}
---
+++ src/app/Models/JobOpportunity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobOpportunity extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobOpportunityFactory> */
+    use HasFactory;
+
+    protected $fillable = [];
+}
